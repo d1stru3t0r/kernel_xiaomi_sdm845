c5b2a1e2a114ec369fde8791056b764adaafa353
shivatejapeddi <shivatejapeddi@gmail.com>
2021-05-06 12:21:31 +0530

    Revert "stop_machine: Make stop_cpus() static"
    This reverts commit fbc6f0194ff01ab51c90b5827343554741e66891.

--- include/linux/stop_machine.h
+++ include/linux/stop_machine.h
@@ -31,6 +31,7 @@ int stop_one_cpu(unsigned int cpu, cpu_stop_fn_t fn, void *arg);
 int stop_two_cpus(unsigned int cpu1, unsigned int cpu2, cpu_stop_fn_t fn, void *arg);
 bool stop_one_cpu_nowait(unsigned int cpu, cpu_stop_fn_t fn, void *arg,
 			 struct cpu_stop_work *work_buf);
+int stop_cpus(const struct cpumask *cpumask, cpu_stop_fn_t fn, void *arg);
 void stop_machine_park(int cpu);
 void stop_machine_unpark(int cpu);
 
@@ -76,6 +77,14 @@ static inline bool stop_one_cpu_nowait(unsigned int cpu,
 	}
 
 	return false;
+}
+
+static inline int stop_cpus(const struct cpumask *cpumask,
+			    cpu_stop_fn_t fn, void *arg)
+{
+	if (cpumask_test_cpu(raw_smp_processor_id(), cpumask))
+		return stop_one_cpu(raw_smp_processor_id(), fn, arg);
+	return -ENOENT;
 }
 
 #endif	/* CONFIG_SMP */
